c1db51b5d5b4dc4aa9b1546610498d745695cbd7
Jason <2029771642@qq.com>
2024-11-30 00:12:28 +0800

    蓝桥云课/蓝桥杯真题/java/No-2060-裁纸刀

---
+++ No_2060.java
@@ -0,0 +1,13 @@
+public class No_2060{
+    public class Main {
+        public static void main(String[] args) {
+            // Scanner scan = new Scanner(System.in);
+            int edge = 4;
+            int row = 20-1;
+            int column = 20*(22-1);
+            int result = edge + row + column;
+            System.out.println(result);
+            // scan.close();
+        }
+    }
+}
