b7bca59cfb91fbb9f5724bef98d3af6dd57559e8
Jason <2029771642@qq.com>
2024-11-29 23:49:05 +0800

    蓝桥云课/蓝桥杯真题/java/No-1446-ASC

---
+++ No_1446.java
@@ -0,0 +1,9 @@
+public class No_1446{
+    public class Main {
+        public static void main(String[] args) {
+            // Scanner scan = new Scanner(System.in);
+            System.out.println((int)'L');
+            // scan.close();
+        }
+    }
+}
